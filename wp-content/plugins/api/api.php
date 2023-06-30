654e432fcc925fb8551b1226af7ea5c5a0a94f9c
Konyuka <michaelsaiba84@gmail.com>
2023-06-30 17:10:44 +0300

    make plugin file

--- wp-content/plugins/api/api.php
+++ wp-content/plugins/api/api.php
@@ -5,11 +5,11 @@ $response = wp_remote_get('https://www.biddetail.com/kenya/C62A8CB5DD405E768CAD7
 $body = wp_remote_retrieve_body($response);
 $data = json_decode($body);
 
-foreach ($data as $item) {
-    $product = new WC_Product_Simple();
-    $product->set_name($item->name);
-    $product->set_price($item->price);
-    $product->save();
-}
+// foreach ($data as $item) {
+//     $product = new WC_Product_Simple();
+//     $product->set_name($item->name);
+//     $product->set_price($item->price);
+//     $product->save();
+// }
 
 ?>
